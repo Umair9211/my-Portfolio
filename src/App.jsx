f1cd114fd18dd3a9ae928dc1221acf08ca1f77b3
Umair9211 <umair.farooq9211@gmail.com>
2025-08-10 12:51:25 +0500

    Added Loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,37 @@ import "./styles/global.css";
 import "./styles/components.css"
 import Network from "./pages/Network.jsx";
 import Cyber from "./pages/Cyber.jsx";
+import Loader from './components/Loader.jsx';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  return (
-    <>
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/web" element={<Web/>} />
-          <Route path="/net" element={<Network/>} />
-          <Route path="/cyber" element={<Cyber/>} />
-        </Route>
-      </Routes>
-    </Router>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+const handleLoad = () => setLoading(false);
+    window.addEventListener('load', handleLoad);
+      return () => window.removeEventListener('load', handleLoad);
+  },[]);
+
+return (
+  <>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/web" element={<Web />} />
+            <Route path="/net" element={<Network />} />
+            <Route path="/cyber" element={<Cyber />} />
+          </Route>
+        </Routes>
+      </Router>
+    )}
+  </>
+);
+
 }
 
 export default App;
